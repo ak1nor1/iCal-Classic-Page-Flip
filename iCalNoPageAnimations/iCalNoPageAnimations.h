388694d282a75ed32feca87f736a168de6d854c6
Lukas Pitschl <lukas@dressyvagabonds.com>
2011-09-21 02:18:29 +0200

    - Initial checkin.

---
+++ iCalNoPageAnimations/iCalNoPageAnimations.h
@@ -0,0 +1,16 @@
+//
+//  iCalNoPageAnimations.h
+//  iCalNoPageAnimations
+//
+//  Created by Lukas Pitschl on 20.09.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface iCalNoPageAnimations : NSObject
+
++ (void)load;
++ (iCalNoPageAnimations*) sharedInstance;
+
+@end
